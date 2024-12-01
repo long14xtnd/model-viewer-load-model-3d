35f9606824b6493ebcebbafe5fbb47b4ebebc43d
long14xtnd <longdoan14xtnd@gmail.com>
2024-12-01 17:53:00 +0700

    chan ko cho user f12

--- main.js
+++ main.js
@@ -47,3 +47,14 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 });
+
+
+//chặn người dùng f12
+document.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+});
+document.addEventListener('keydown', (e) => {
+  if (e.ctrlKey && (e.key === 'u' || e.key === 'U' || e.key === 's' || e.key === 'S') || e.key === 'F12') {
+    e.preventDefault();
+  }
+});
